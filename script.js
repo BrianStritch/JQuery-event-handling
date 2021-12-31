1dd80f612614436df1858ace23285580254aaa01
BrianStritch <brians12ca18@gmail.com>
2021-12-31 16:07:10 +0000

    add ci challenge code for event handling

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
  
 
- 	$("#stream1_btn").on("click", function() {
+ 	$("#stream1_btn").on("click", function() {   // you can also use .click(function.....) instead of on.('click',function .....)
  		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
 	  	$(".stream1").addClass('highlight_stream');
 	});
-	$("#stream2_btn").on("click", function() {
+	$("#stream2_btn").on("click", function() { // you can also add mouse interactions as in .mouseover or .mouseleave
 		$(".stream1").removeClass('highlight_stream');
 		$(".stream2").removeClass('highlight_stream');
 		$(".stream3").removeClass('highlight_stream');
